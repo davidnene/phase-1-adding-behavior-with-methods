4bf2028844b3fc0229ef2191f4a58b97a62ef956
davidnene <davienesh4@gmail.com>
2022-07-04 14:32:52 +0300

    commit tree 696ef4839e5e0f4b515ebfb4347f56b62836128c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog extends Cat {
+    speak () {
+        return `${this.name} says woof!`
+    }
+}
+
+
+
+class Bird extends Dog {
+    speak() {
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`;
+        } else if (this.sex !== 'male') {
+            return `${this.name} says squawk!`
+        }
+    }
+}
+
+let parr = new Bird('par','fmale')
+console.log(parr.speak())
